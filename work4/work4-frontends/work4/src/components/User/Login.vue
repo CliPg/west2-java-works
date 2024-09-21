cc77bfeac34010e8ef56bfb191e86e97e3c255be
CliPg <775075825@qq.com>
2024-09-21 12:16:43 +0800

    实现登录，但还未实现保存状态

---
+++ work4/work4-frontends/work4/src/components/User/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h2>登录</h2>
+      <input v-model="username" placeholder="用户名" />
+      <input v-model="password" placeholder="密码" type="password" />
+      <button @click="handleLogin">登录</button>
+    </div>
+  </template>
+
+  <script>
+  import { login } from '@/api/user';
+
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      async handleLogin() {
+        try {
+          const response = await login({ username: this.username, password: this.password });
+          console.log('登录成功', response.data);
+          // 登录成功后处理
+        } catch (error) {
+          console.error('登录失败', error);
+        }
+      }
+    }
+  };
+  </script>
+  
